735051bff8e9e6ebce92fe08382e5de9f6559957
rajanimation <pntkiran@gmail.com>
2017-03-12 17:12:08 -0400

    nginx provisioning

--- provision.sh
+++ provision.sh
@@ -1,3 +1,8 @@
 apt-get -y update
 apt-get -y install nginx
+
+# remove site content from nginx default location
+rm -rf /usr/share/nginx/www
+ln -s /vagrant/www /usr/share/nginx/www
+
 service nginx start
