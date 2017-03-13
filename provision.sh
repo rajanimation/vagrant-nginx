d9ff5ec323c90f6cf0c3a81984e5c79f4ba2215e
rajanimation <pntkiran@gmail.com>
2017-03-13 00:07:37 -0400

    provisioning nginx config

--- provision.sh
+++ provision.sh
@@ -2,7 +2,10 @@ apt-get -y update
 apt-get -y install nginx
 
 # remove site content from nginx default location
-rm -rf /usr/share/nginx/www
-ln -s /vagrant/www /usr/share/nginx/www
+#rm -rf /usr/share/nginx/www
+#ln -s /vagrant/www /usr/share/nginx/www
+
+rm -rf /etc/nginx/sites-enabled
+cp -r /vagrant/sites-enabled /etc/nginx
 
 service nginx start
